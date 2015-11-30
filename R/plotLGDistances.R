4086c2e15ee3346b52c369244667da757a9661a6
koneill <koneill@bcgsc.ca>
2015-11-30 15:32:42 -0800

    Remove hard specification of breaks. First, this was crashing, since there were two 12/15s, second, this is not the right way to generate breaks, third, the need for hard-coded breaks in the colours isn't quite clear?

--- R/plotLGDistances.R
+++ R/plotLGDistances.R
@@ -6,9 +6,8 @@ plotLGDistances.func <- function(object, allStrands)
   sim <- 1-as.matrix(daisy(data.frame(linkageStrands)))
   rownames(sim) <- rownames(linkageStrands)
   colnames(sim) <- rownames(linkageStrands)
-  breaks <- c(0/15, 1/15, 2/15, 3/15, 4/15, 5/15, 6/15, 7/15, 8/15, 9/15, 10/15, 11/15, 12/15, 12/15, 14/15, 15/15)
   cols <- c("cyan","cyan3","blue","blue4","gray22","gray0","gray0","gray0","gray0","gray0","gray22","red4","red3","red","darkorange")
-  heatmap.2(sim, trace='none', col=cols, breaks=breaks)	
+  heatmap.2(sim, trace='none', col=cols)
 }
 ####################################################################################################
 #' plotLGDistances -- plots a heatmap of the distances between linkage groups 
