20ea03c5f8245890824453e5d39ef9f5b41a7d79
koneill <koneill@bcgsc.ca>
2015-12-08 14:51:05 -0800

    examples

--- inst/examples/clusterContigs.R
+++ inst/examples/clusterContigs.R
@@ -1,10 +1,10 @@
-data(exampleWWCCMatrix)
-data(exampleWCMatrix)
-
-clusteredContigs <- clusterContigs(exampleWWCCMatrix, verbose=FALSE)
-show(clusteredContigs)
-show(clusteredContigs[[1]])
-
-LGOrientations <- reorientLinkageGroups(clusteredContigs, exampleWWCCMatrix)
-reorientedMatrix <- reorientStrandTable(exampleWCMatrix, linkageGroups = clusteredContigs, orientation = LGOrientations)
-mergedLinkageGroups <- mergeLinkageGroups(clusteredContigs,reorientedMatrix)
+# data(exampleWWCCMatrix)
+# data(exampleWCMatrix)
+# 
+# clusteredContigs <- clusterContigs(exampleWWCCMatrix, verbose=FALSE)
+# show(clusteredContigs)
+# show(clusteredContigs[[1]])
+# 
+# LGOrientations <- reorientLinkageGroups(clusteredContigs, exampleWWCCMatrix)
+# reorientedMatrix <- reorientStrandTable(exampleWCMatrix, linkageGroups = clusteredContigs, orientation = LGOrientations)
+# mergedLinkageGroups <- mergeLinkageGroups(clusteredContigs,reorientedMatrix)
