a2dd092fdda3b079560dcb9738efc32281391c0b
koneill <koneill@bcgsc.ca>
2016-01-29 12:07:42 -0800

    Change over to using BiocParallel instead of snow.

--- R/mergeLinkageGroups.R
+++ R/mergeLinkageGroups.R
@@ -1,5 +1,5 @@
 
-mergeLinkageGroups.func <- function(object, allStrands, clusNum=NULL, cluster=1, similarityCutoff=0.7)
+mergeLinkageGroups.func <- function(object, allStrands, clusterParam=NULL, cluster=1, similarityCutoff=0.7)
 {
 
   consensusStrands <- data.frame(do.call(rbind, lapply(object, computeConsensus, allStrands)))
@@ -9,21 +9,23 @@ mergeLinkageGroups.func <- function(object, allStrands, clusNum=NULL, cluster=1,
 
   consensusStrands <- new('StrandStateMatrix', consensusStrands)
 
-  if(!(is.null(clusNum)))
-  {
-    slaveNum <- makeCluster(clusNum)
-    consensus.groups <- clusterContigs(consensusStrands, snowCluster=slaveNum, recluster=cluster, randomise=TRUE, similarityCutoff=similarityCutoff, verbose=FALSE)
-    stopCluster(slaveNum)
-  }else{
-    consensus.groups <- clusterContigs(consensusStrands, recluster=cluster, randomise=TRUE, similarityCutoff=similarityCutoff, verbose=FALSE)
-  }
+  consensus.groups <- clusterContigs(consensusStrands, 
+  								   clusterParam=clusterParam, 
+  								   recluster=cluster, 
+  								   randomise=TRUE, 
+  								   similarityCutoff=similarityCutoff, 
+  								   verbose=FALSE)
 
  
   mergeThoseGroups <- lapply(consensus.groups, function(x) as.character(melt(object[x])[,1]))
 
   mergeThoseGroups <- new('LinkageGroupList', 
                           mergeThoseGroups[order(sapply(mergeThoseGroups, length), decreasing=TRUE)], 
-                          names= sapply(1:length(mergeThoseGroups), function(x){paste('LG', x, ' (', length(mergeThoseGroups[[x]]), ')', sep='')}) )
+                          names= sapply(1:length(mergeThoseGroups), 
+                          			  function(x)
+                          			  	{
+                          			  	paste('LG', x, ' (', length(mergeThoseGroups[[x]]), ')', sep='')
+                          			  	}))
 
   return(mergeThoseGroups)
 }
@@ -31,7 +33,8 @@ mergeLinkageGroups.func <- function(object, allStrands, clusNum=NULL, cluster=1,
 #' mergeLinkageGroups -- merge very similar linkage groups, including those in reverse orientation
 #' @param object LinkageGroupList 
 #' @param allStrands StrandStateMatrix for all linkageGroups (usually reoriented by reorientStrandTable)
-#' @param clusNum  Number of parallel processors to use when clustering contigs. Default is NULL. 
+#' @param clusterParam  optional \code{BiocParallelParam} specifying cluster to use for parallel execution.
+#' When \code{NULL}, execution will be serial.
 #' @param cluster  Integer denoting the number of reclusterings to be performed for creating linkage groups (default is 1)
 #' @param similarityCutoff merge contigs that are more similar this this
 #' @return list of indices within the allStrands matrix indicating linkage group membership,  
@@ -41,7 +44,7 @@ mergeLinkageGroups.func <- function(object, allStrands, clusNum=NULL, cluster=1,
 #' @rdname mergeLinkageGroups
 #' @example inst/examples/mergeLinkageGroups.R
 #' @export
-#' @import snow
+#' @import BiocParallel
 #' @importFrom reshape2 melt 
 #' @include AllClasses.R
 ####################################################################################################
